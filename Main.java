ae91bd118188d00da87c47ff4d9440689b0eb1f9
José Ribamar Pilar de Araújo Neto <45271784+joseRibamar21@users.noreply.github.com>
2022-08-01 23:42:05 -0300

    Menu Criado #8
    Foi criado menu

--- Main.java
+++ Main.java
@@ -3,9 +3,31 @@ import java.util.Arrays;
 public class Main {
 
 	public static void main(String[] args) {
-		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Conta", "Cliente", "Operacoes"));
-		System.out.println(mainMenu.getSelection() + "foi selecionada");
-		System.out.println("Fim");
+		Menu mainMenu =  new Menu("Menu Principal", Arrays.asList("Cadastrar Cliente",
+		 "Abrir Conta", "Realizar Saque","Realizar Deposito","Ver Saldo", "Extrato", "Sair"));
+
+		while (true) {
+			switch (mainMenu.getSelection()) {
+				case 1:
+					break;
+				case 2:
+					break;
+				case 3:
+					break;
+				case 4:
+					break;
+				case 5:
+					break;
+				case 6:
+					break;		
+				case 7:
+				System.exit(0);
+					break;	
+				default:
+					break;
+			 }
+		}
+		
 	}
 
 }
